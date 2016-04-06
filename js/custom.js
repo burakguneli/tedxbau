68361a0ad5907735e145f21f0422f5c703c4be02
Mert <pvtmert@gmail.com>
2016-04-06 23:17:19 +0300

    updated: Wed Apr  6 23:17:16 EEST 2016

--- js/custom.js
+++ js/custom.js
@@ -73,8 +73,6 @@ window.onload = function() {
 			$(this).toggleClass('fs');
 		};
 	*/
-	//if(1459382400000 > Date.now() && 1459641600000 < Date.now())
-		document.getElementsByClassName("ticket")[0].style.display = "none";
 	return;
 };
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
